66c2fa1bf9005d364103298530e7daf351c6edc1
Enrico <enrico.melis.casa@gmail.com>
2025-12-10 11:16:10 +0100

    added custom config for mappings, options and clang for lazyvim

--- .config/nvim/lua/plugins/example.lua
+++ .config/nvim/lua/plugins/example.lua
@@ -1,14 +1,6 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if true then return {} end
-
--- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
---
--- In your plugin files, you can:
--- * add extra plugins
--- * disable/enabled LazyVim plugins
--- * override the configuration of LazyVim plugins
-return {
+if true then return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
 
@@ -18,8 +10,46 @@ return {
     opts = {
       colorscheme = "gruvbox",
     },
+ {
+  "nvim-mini/mini.surround",
+  keys = function(_, keys)
+    -- Populate the keys based on the user's options
+    local opts = LazyVim.opts("mini.surround")
+    local mappings = {
+      { opts.mappings.add, desc = "Add Surrounding", mode = { "n", "x" } },
+      { opts.mappings.delete, desc = "Delete Surrounding" },
+      { opts.mappings.find, desc = "Find Right Surrounding" },
+      { opts.mappings.find_left, desc = "Find Left Surrounding" },
+      { opts.mappings.highlight, desc = "Highlight Surrounding" },
+      { opts.mappings.replace, desc = "Replace Surrounding" },
+      { opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
+    }
+    mappings = vim.tbl_filter(function(m)
+      return m[1] and #m[1] > 0
+    end, mappings)
+    return vim.list_extend(mappings, keys)
+  end,
+  opts = {
+    mappings = {
+      add = "gsa", -- Add surrounding in Normal and Visual modes
+      delete = "gsd", -- Delete surrounding
+      find = "gsf", -- Find surrounding (to the right)
+      find_left = "gsF", -- Find surrounding (to the left)
+      highlight = "gsh", -- Highlight surrounding
+      replace = "gsr", -- Replace surrounding
+      update_n_lines = "gsn", -- Update `n_lines`
+    },
   },
+} },
+} end
 
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
   -- change trouble config
   {
     "folke/trouble.nvim",
@@ -98,7 +128,7 @@ return {
       },
       -- you can do any additional lsp server setup here
       -- return true if you don't want this server to be setup with lspconfig
-      ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+      ---@type table<string, fun(server:string, opts:lspconfig.options):boolean?>
       setup = {
         -- example to setup with typescript.nvim
         tsserver = function(_, opts)
