e6e4ac1c4f0372a2de33afdd98b48cc14d43a897
Enrico <enrico.melis.casa@gmail.com>
2025-12-14 20:34:39 +0100

    added autocmd to disable formatting in .md files

--- .config/nvim/lua/melis/core/autocmd.lua
+++ .config/nvim/lua/melis/core/autocmd.lua
@@ -4,5 +4,11 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
-
-vim.cmd('autocmd BufWritePre * lua vim.lsp.buf.format({ async = false })')
+vim.api.nvim_create_autocmd("BufWritePre", {
+	-- Exclude md, but include all others that have a formatter.
+	callback = function(args)
+		if vim.bo[args.buf].filetype ~= "markdown" then
+			vim.lsp.buf.format({ async = true })
+		end
+	end,
+})
