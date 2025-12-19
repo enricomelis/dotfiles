f5b5043d9536290d2451ff8aa871f27e5ac28554
Enrico <enrico.melis.casa@gmail.com>
2025-12-19 12:10:01 +0100

    changed config to allow 50% transparency (only in buffers)

--- .config/nvim/lua/melis/plugins/colorscheme.lua
+++ .config/nvim/lua/melis/plugins/colorscheme.lua
@@ -1,9 +1,17 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  config = function()
-    vim.cmd.colorscheme "tokyonight-storm"
-  end
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {},
+	config = function()
+		local tokyonight = require("tokyonight")
+		tokyonight.setup({
+			transparent = true,
+			styles = {
+				sidebars = "transparent",
+				-- floats = "transparent",
+			},
+		})
+		vim.cmd.colorscheme("tokyonight-storm")
+	end,
 }
