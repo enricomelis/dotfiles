076fa9bfdca65b06b9b2f377dc77d4cb0b261afd
Enrico <enrico.melis.casa@gmail.com>
2025-12-19 11:55:56 +0100

    added conform.nvim config

--- .config/nvim/lua/melis/plugins/conform.lua
+++ .config/nvim/lua/melis/plugins/conform.lua
@@ -1,11 +1,28 @@
 return {
 	"stevearc/conform.nvim",
-	opts = {},
+	event = "BufWritePre",
+	cmd = { "ConformInfo" },
+	opts = {
+		formatters_by_ft = {
+			markdown = { "prettier" },
+			lua = { "stylua" },
+			javascript = { "prettier", "eslint_d" },
+		},
+
+		format_on_save = {
+			lsp_fallback = true,
+			timeout_ms = 500,
+			async = false, -- Synchronous is often safer for BufWritePre
+		},
+
+		formatters = {
+			clangd_format = {
+				args = { "--style", "file" },
+			},
+		},
+	},
+
 	config = function()
-		local conform = require("conform")
-		conform.format({
-			formatters = { "clangd_format" },
-			timeout_ms = 1000,
-		})
+		require("conform").setup()
 	end,
 }
